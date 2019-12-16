c59f06780171f88c059238885393d5b7f2ac7d42
Natalie Tapias <nataliemtapias@gmail.com>
2019-12-16 14:17:01 -0800

    started router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <div className="app">
+        <Router>
+          <header className="App-header">
+            <h1>Kingrey & Tapias VideoStore</h1>
+            <div>
+              <nav>
+                <ul>
+                  <li>
+                    <Link to="/">Movies</Link>
+                  </li>
+                  <li>
+                    <Link to="/customers">Customers</Link>
+                  </li>
+                  <li>
+                    <Link to="/search">Search</Link>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </header>
+        </Router>
+      </div>  
     );
   }
 }
