57a4b41e6bc2bc4efa6589c94c020b41b8a4bf3d
Natalie Tapias <nataliemtapias@gmail.com>
2019-12-17 09:16:53 -0800

    search bar scaffolding

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,50 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: ''
+    }
+  }
+
+  handleChange = (event) => {
+    const newState = {};
+    newState.searchTerm = event.target.value;
+    this.setState(newState);
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    // this is where we submit the api request & a callback function?
+    console.log('handleSubmit', this.state.searchTerm);
+    this.setState = {
+      searchTerm: ''
+    };
+  };
+
+
   render() {
     return (
-    <p>search</p>
+    <form onSubmit={this.handleSubmit}>
+      <div>
+        <label>Search for a video: </label>
+        <textarea
+          value={this.state.searchTerm}
+          placeholder="enter search term here"
+          name="searchbar"
+          onChange={this.handleChange} />
+      </div>
+
+      <div>
+        <input 
+          type="submit" 
+          value="Submit" 
+          onClick={this.handleSubmit}/>
+      </div>
+
+    </form>
     );
     }
   };
