80faada685f1826db8e88a4e3e1134e9a6655886
Natalie Tapias <nataliemtapias@gmail.com>
2019-12-18 11:20:04 -0800

    destructure movie props

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import './Movie.css';
 
-const Movie = (props) => {
+const Movie = ({title, image_url, overview, release_date, selectMovieCallback, id}) => {
 
     return (
       <div class="col-sm-4">
         <div className="card">
-          <h3 className="card-title">{props.title}</h3>
+          <h3 className="card-title">{title}</h3>
           <div className="card-body">
-            <img src={props.image_url} alt={props.title} /> 
-            <p className="card-text">{props.overview} Released on: {props.release_date}</p>
+            <img src={image_url} alt={title} /> 
+            <p className="card-text">{overview} Released on: {release_date}</p>
             <button
               className="btn btn-primary movie--select-movie-btn"
-              onClick={() => { props.selectMovieCallback(props.id) }}>
+              onClick={() => { selectMovieCallback(id) }}>
               Select
             </button>
           </div>
