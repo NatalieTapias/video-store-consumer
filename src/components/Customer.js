0656661c7b9bc559546f78d4f1a2a750e6618311
Natalie Tapias <nataliemtapias@gmail.com>
2019-12-18 11:17:20 -0800

    destructuring customer props

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import './Customer.css';
 
-const Customer = (props) => {
+const Customer = ({name, account_credit, movies_checked_out_count, id, selectCustomerCallback}) => {
   
     return (
       <div className="col-sm-4">
         <div className="card">
-          <h3 className="card-title">{props.name}</h3>
+          <h3 className="card-title">{name}</h3>
           <div className="card-body">
-            <p>Customer ID: {props.id}</p>
-            <p>Account Credit: ${props.account_credit}</p>
-            <p>movies checked out: {props.movies_checked_out_count}</p>
+            <p>Customer ID: {id}</p>
+            <p>Account Credit: ${account_credit}</p>
+            <p>movies checked out: {movies_checked_out_count}</p>
             <button 
               className="btn btn-primary"
-              onClick={() => {props.selectCustomerCallback(props.id)}}>Select Customer</button></div>
+              onClick={() => {selectCustomerCallback(id)}}>Select Customer</button></div>
         </div>
       </div>
     
