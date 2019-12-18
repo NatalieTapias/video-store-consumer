16c7f3c42bc7e3f10c8ebbd5b41adec16dc9b888
Natalie Tapias <nataliemtapias@gmail.com>
2019-12-18 14:31:05 -0800

    Clicking on a Movie component will toggle between show details and hide details

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,25 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const Movie = ({title, image_url, overview, release_date, selectMovieCallback, id}) => {
+class Movie extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      showDetails: false,
+    }
+  };
+
+  toggleShowDetails = () => {
+    let doesItShowDetails = this.state.showDetails;
+    doesItShowDetails = !(this.state.showDetails);
+    this.setState({showDetails: doesItShowDetails});
+  }
+
+  render() {
+    const {title, image_url, overview, release_date, selectMovieCallback, id} = this.props;
 
     return (
       <div className="col-sm-4">
-        <div className="card">
-          <h3 className="card-title">{title}</h3>
-          <div className="card-body">
-            <img src={image_url} alt={title} /> 
-            <p className="card-text">{overview}</p>
-            <p>Released on: {release_date}</p>
-            <button
-              className="btn btn-primary"
-              onClick={() => { selectMovieCallback(id) }}>
-              Select
-            </button>
-          </div>
-        </div>      
+        <a onClick={this.toggleShowDetails}>
+          <div className="card">
+            <h3 className="card-title">{title}</h3>
+            <div className="card-body">
+              <img src={image_url} alt={title} /> 
+              {this.state.showDetails && ( 
+                <>
+                <p className="card-text">{overview}</p>
+                <p>Released on: {release_date}</p>
+                </>
+              )}
+              <button
+                className="btn btn-primary"
+                onClick={() => { selectMovieCallback(id) }}>
+                Select
+              </button>
+            </div>
+          </div> 
+        </a>     
       </div>
-    )
+      );
+    }
   };
 
 export default Movie;
